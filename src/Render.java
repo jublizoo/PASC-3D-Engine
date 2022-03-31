a38acaebc7bf8438f4b5ca1b7fc88806eed53aaa
Julian Hennessy <jjhennessy1231@gmail.com>
2022-03-31 14:11:13 -0500

    Multithreading is now functional but buggy. Random triangles will dissapear, and there appears to be an issue with z-buffer (maybe something else), which causes a weird flickering.

--- src/Render.java
+++ src/Render.java
@@ -16,44 +16,108 @@ public class Render extends JPanel implements Runnable{
 	Main m;
 	Double[][] zBuffer;
 	
+	Graphics2D g2d;
+	
 	BufferedImage texture;
 	BufferedImage finalImg;
+	
+	ArrayList<Thread> threads;
+	ArrayList<BufferedImage> imageSections;
+	ArrayList<Double[]> corner1s;
+	ArrayList<Double[]> corner2s;
 
 	public Render(Main m) {
 		p = new Projection(m);
 		this.m = m;
 		loadImages();
+		
 	}
 	
 	@Override
-	public void run() {
-		BufferedImage imageSection;
-		
-		switch(Thread.currentThread().getName()){
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		}
+	public void run() {		
+		textureTriangles(g2d);
 	}
 	
+	@Override
 	protected void paintComponent(Graphics g) {
 		//p.setDisplayDimensions();
-		Graphics2D g2d = (Graphics2D) g;
+		this.g2d = (Graphics2D) g;
+		
+		//
 		g2d.setColor(Color.WHITE);
 		g2d.fillRect(0, 0, p.screenWidth, p.screenHeight);
 		g2d.setColor(Color.LIGHT_GRAY);
 		g2d.fillRect((int) p.startX, (int) p.startY, (int) p.innerWidth, (int) p.innerHeight);
 		g2d.setColor(Color.BLACK);
-		//drawTriangles(g2d);
-		textureTriangles(g2d);
-		//drawPoints(g2d);
-	}
 		
+		initializeThreads();
+		
+		try {
+			for(Thread thread : threads) {
+				thread.join();
+			}
+		}catch(Exception e) {
+			System.out.println("Could not join threads");
+		}
+		
+		drawImages(g2d);
+		
+	}
+	
+	public void initializeThreads() {
+		threads = new ArrayList<Thread>();
+		
+		for(int i = 0; i < 4; i++) {
+			threads.add(new Thread(this, "" + i));
+		}
+		
+		imageSections = new ArrayList<BufferedImage>();
+		
+		for(int i = 0; i < threads.size(); i++) {
+			if(p.innerWidth != 0) {
+				imageSections.add(new BufferedImage((int) (p.innerWidth / 2.0), (int) (p.innerHeight / 2.0), BufferedImage.TYPE_INT_ARGB));
+			}else {
+				System.out.println("bals");
+				return;
+			}
+		}
+		
+		corner1s = new ArrayList<Double[]>();
+		
+		corner1s.add(new Double[] {0.0, 0.0});
+		corner1s.add(new Double[] {Math.floor(p.innerWidth / 2), 0.0});
+		corner1s.add(new Double[] {0.0, Math.floor(p.innerHeight / 2)});
+		corner1s.add(new Double[] {Math.floor(p.innerWidth / 2), Math.floor(p.innerHeight / 2)});
+		
+		corner2s = new ArrayList<Double[]>();
+		
+		for(int i = 0; i < corner1s.size(); i++) {
+			corner2s.add(new Double[] {Math.floor(corner1s.get(i)[0] + p.innerWidth / 2), Math.floor(corner1s.get(i)[1] + p.innerHeight / 2)});
+		}
+		
+		for(Thread t : threads) {
+			t.start();
+		}
+	}
+	
+	//Draws the corresponding images for each thread
+	public void drawImages(Graphics g2d) {
+		int x;
+		int y;
+		int width = (int) (p.innerWidth / 2.0);
+		int height = (int) (p.innerHeight / 2.0);
+		for(int i = 0; i < imageSections.size(); i++) {
+			x = (int) (double) (p.startX + corner1s.get(i)[0]);
+			y = (int) (double) (p.startY + corner1s.get(i)[1]);
+			
+			g2d.drawImage(imageSections.get(i), x, y, width, height, null);
+		}
+	}
+	
 	public void loadImages() {
 		try {
 			texture = ImageIO.read(new File("download.png"));
-		} catch (IOException e) {}
+		} catch (IOException e) {	}
 	}
 	
 	//TODO Check if each point is within the inner window
@@ -79,153 +143,145 @@ public class Render extends JPanel implements Runnable{
 	}	
 	
 	//Corner 1 is the topleft corner. Corner 2 is the bottomright corner
-		public ArrayList<Double[][]> clipTriangle(Double[][] originalTriangle, Double[] corner1, Double[] corner2) {
-			ArrayList<Double[][]> clippedTriangles = new ArrayList<Double[][]>();
-			clippedTriangles.add(originalTriangle);
-			//The starting triangle (may have already been clipped on other boundaries)
-			Double[][] triangle = null;
-			//The triangle after being clipped on the current boundary
-			Double[][] clippedTriangle = new Double[3][2];
-			
-			//If a point is inside the bounding box
-			boolean inside;
-			int[] insidePoints;
-			int insideSize;
-			int[] outsidePoints;
-			int outsideSize;
-			
-			//initial size of the clipped triangle array 
-			int initialSize;
-			
-			//The 2 intersections with the current line (if it intersects)
-			Double[][] intersections;
+	public ArrayList<Double[][]> clipTriangle(Double[][] originalTriangle, Double[] corner1, Double[] corner2) {
+		ArrayList<Double[][]> clippedTriangles = new ArrayList<Double[][]>();
+		clippedTriangles.add(originalTriangle);
+		//The starting triangle (may have already been clipped on other boundaries)
+		Double[][] triangle = null;
+		//The triangle after being clipped on the current boundary
+		Double[][] clippedTriangle = new Double[3][2];
+		
+		//If a point is inside the bounding box
+		boolean inside;
+		int[] insidePoints;
+		int insideSize;
+		int[] outsidePoints;
+		int outsideSize;
+		
+		//initial size of the clipped triangle array 
+		int initialSize;
+		
+		//The 2 intersections with the current line (if it intersects)
+		Double[][] intersections;
+				
+		for(int i = 0; i < 4; i++) {
+			//We only want to cycle to the initial size. We will be adding new triangles, and if we use the actual size, it will continue removing the new triangles.
+			initialSize = clippedTriangles.size();
+			for(int b = 0; b < initialSize; b++) {
+				insidePoints = new int[3];
+				insideSize = 0;
+				outsidePoints = new int[3];
+				outsideSize = 0;
+				
+				triangle = clippedTriangles.get(0);
+				clippedTriangles.remove(0);
+				
+				for(int c = 0; c < 3; c++) {
+					inside = false;
 					
-			for(int i = 0; i < 4; i++) {
-				//We only want to cycle to the initial size. We will be adding new triangles, and if we use the actual size, it will continue removing the new triangles.
-				initialSize = clippedTriangles.size();
-				for(int b = 0; b < initialSize; b++) {
-					insidePoints = new int[3];
-					insideSize = 0;
-					outsidePoints = new int[3];
-					outsideSize = 0;
-					
-					triangle = clippedTriangles.get(0);
-					clippedTriangles.remove(0);
-					
-					for(int c = 0; c < 3; c++) {
-						inside = false;
-						
-						switch(i) {
-						//top
-						case 0:
-							if(triangle[c][1] > corner1[1]) {
-								inside = true;
-							}
-							break;
-						//bottom
-						case 1:
-							if(triangle[c][1] < corner2[1]) {
-								inside = true;
-							}
-							break;
-						//left
-						case 2:
-							if(triangle[c][0] > corner1[0]) {
-								inside = true;
-							}
-							break;
-						//right
-						case 3:
-							if(triangle[c][0] < corner2[0]) {
-								inside = true;
-							}
-							break;
+					switch(i) {
+					//top
+					case 0:
+						if(triangle[c][1] > corner1[1]) {
+							inside = true;
 						}
-						
-						if(inside) {
-							insidePoints[insideSize] = (c); 
-							insideSize++;
-						}else {
-							outsidePoints[outsideSize] = (c); 
-							outsideSize++;
+						break;
+					//bottom
+					case 1:
+						if(triangle[c][1] < corner2[1]) {
+							inside = true;
 						}
+						break;
+					//left
+					case 2:
+						if(triangle[c][0] > corner1[0]) {
+							inside = true;
+						}
+						break;
+					//right
+					case 3:
+						if(triangle[c][0] < corner2[0]) {
+							inside = true;
+						}
+						break;
 					}
 					
-					intersections = new Double[2][2];
-					
-					//If a triangle is outside (outsideSize == 3), we do nothing. The triangle is already removed.
-					if(outsideSize == 2) {						
-						switch(i) {
-						//top
-						case 0:
-							intersections = collision(corner1[1], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], true);
-							break;
-						//bottom
-						case 1:
-							intersections = collision(corner2[1], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], true);
-							break;
-						//left
-						case 2:
-							intersections = collision(corner1[0], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], false);
-							break;
-						//right
-						case 3:
-							intersections = collision(corner2[0], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], false);
-							break;
-						}
-						
-						if(intersections[0][0] == null || intersections[1][0] == null || intersections[0][1] == null || intersections[1][1] == null) {
-							System.out.println("big balls");
-						}
-												
-						clippedTriangle[0] = triangle[insidePoints[0]];
-						clippedTriangle[1] = intersections[0];
-						clippedTriangle[2] = intersections[1];
-						clippedTriangles.add(clippedTriangle.clone());
-					}else if(outsideSize == 1) {
-						switch(i) {
-						//top
-						case 0:	
-							intersections = collision(corner1[1], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], true);
-							break;
-						//bottom
-						case 1:
-							intersections = collision(corner2[1], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], true);
-							break;
-						//left
-						case 2:
-							intersections = collision(corner1[0], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], false);
-							break;
-						//right
-						case 3:
-							intersections = collision(corner2[0], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], false);
-							break;
-						}
-						
-						if(intersections[0][0] == null || intersections[1][0] == null || intersections[0][1] == null || intersections[1][1] == null) {
-							System.out.println("big balls");
-						}
-						
-						clippedTriangle[0] = triangle[insidePoints[0]];
-						clippedTriangle[1] = intersections[0];
-						clippedTriangle[2] = intersections[1];
-						clippedTriangles.add(clippedTriangle.clone());
-						
-						//The commented line is uneccesary because it already has that value.
-						clippedTriangle[0] = triangle[insidePoints[0]];
-						clippedTriangle[1] = triangle[insidePoints[1]];
-						clippedTriangle[2] = intersections[1];
-						clippedTriangles.add(clippedTriangle.clone());
-					}else if(outsideSize == 0){
-						//If the triangle is fully inside, we want to add it back (we already removed it)
-						clippedTriangles.add(triangle.clone());
+					if(inside) {
+						insidePoints[insideSize] = (c); 
+						insideSize++;
+					}else {
+						outsidePoints[outsideSize] = (c); 
+						outsideSize++;
 					}
 				}
-			}	
+				
+				intersections = new Double[2][2];
+				
+				//If a triangle is outside (outsideSize == 3), we do nothing. The triangle is already removed.
+				if(outsideSize == 2) {						
+					switch(i) {
+					//top
+					case 0:
+						intersections = collision(corner1[1], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], true);
+						break;
+					//bottom
+					case 1:
+						intersections = collision(corner2[1], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], true);
+						break;
+					//left
+					case 2:
+						intersections = collision(corner1[0], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], false);
+						break;
+					//right
+					case 3:
+						intersections = collision(corner2[0], triangle[outsidePoints[0]], triangle[outsidePoints[1]], triangle[insidePoints[0]], false);
+						break;
+					}
+											
+					clippedTriangle[0] = triangle[insidePoints[0]];
+					clippedTriangle[1] = intersections[0];
+					clippedTriangle[2] = intersections[1];
+					clippedTriangles.add(clippedTriangle.clone());
+				}else if(outsideSize == 1) {
+					switch(i) {
+					//top
+					case 0:	
+						intersections = collision(corner1[1], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], true);
+						break;
+					//bottom
+					case 1:
+						intersections = collision(corner2[1], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], true);
+						break;
+					//left
+					case 2:
+						intersections = collision(corner1[0], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], false);
+						break;
+					//right
+					case 3:
+						intersections = collision(corner2[0], triangle[insidePoints[0]], triangle[insidePoints[1]], triangle[outsidePoints[0]], false);
+						break;
+					}
+					
+					clippedTriangle[0] = triangle[insidePoints[0]];
+					clippedTriangle[1] = intersections[0];
+					clippedTriangle[2] = intersections[1];
+					clippedTriangles.add(clippedTriangle.clone());
+					
+					//The commented line is uneccesary because it already has that value.
+					clippedTriangle[0] = triangle[insidePoints[0]];
+					clippedTriangle[1] = triangle[insidePoints[1]];
+					clippedTriangle[2] = intersections[1];
+					clippedTriangles.add(clippedTriangle.clone());
+				}else if(outsideSize == 0){
+					//If the triangle is fully inside, we want to add it back (we already removed it)
+					clippedTriangles.add(triangle.clone());
+				}
+			}
+		}	
+		
+		return clippedTriangles;
 			
-			return clippedTriangles;
-			
-		}
+	}
 	
 	//Triangle collision, p3 is the point that touches both lines which will be tested for collision
 	Double[][] collision(Double l1, Double[] p1, Double[] p2, Double[] p3, boolean horizontal){
@@ -270,6 +326,8 @@ public class Render extends JPanel implements Runnable{
 	
 	//Tex coords are not sorted
 	public void textureTriangles(Graphics2D g2d) {
+		int threadNumber;
+		
 		//The surface normal of a given triangle
 		Double[] triangleVector;
 		Double[] viewerVector;
@@ -279,8 +337,6 @@ public class Render extends JPanel implements Runnable{
 		
 		Double[][] triangle;
 		ArrayList<Double[][]> clippedTriangles;
-		Double[] corner1 = new Double[] {0.0, 0.0};
-		Double[] corner2 = new Double[] {p.innerWidth, p.innerHeight};
 		
 		p.calculateTriangleMidPoints();
 		p.calculateMidPointDistances();
@@ -289,8 +345,6 @@ public class Render extends JPanel implements Runnable{
 		
 		//Update z-buffer size / reset values to null
 		zBuffer = new Double[(int) p.innerWidth][(int) p.innerHeight];
-		finalImg = new BufferedImage((int) p.innerWidth, (int) p.innerHeight, BufferedImage.TYPE_INT_ARGB);
-
 		
 		for(int a = 0; a < p.triangles2d.size(); a++) {
 			
@@ -298,23 +352,28 @@ public class Render extends JPanel implements Runnable{
 			triangleVector = p.calculateVector(p.triangles3d.get(a));
 			angle2 = p.calculateVectorAngle(triangleVector, viewerToTriangleVector);
 						
-			if(p.midPointDistances.get(a) > 0 && Math.abs(angle2) > Math.PI / 2) {			
+			//if(p.midPointDistances.get(a) > 0 && Math.abs(angle2) > Math.PI / 2) {			
 				viewerVector = new Double[] {-Math.sin(p.viewerAngle[0]), Math.cos(p.viewerAngle[0]), 0.0};
 				angle = p.calculateVectorAngle(triangleVector, viewerVector);
 				
-				clippedTriangles = clipTriangle(p.triangles2d.get(a), corner1, corner2);
+				threadNumber = Integer.parseInt(Thread.currentThread().getName());
+				clippedTriangles = clipTriangle(p.triangles2d.get(a), corner1s.get(threadNumber), corner2s.get(threadNumber));
+				//clippedTriangles = new ArrayList<Double[][]>();
+				//clippedTriangles.add(p.triangles2d.get(a));
 				
 				for(int b = 0; b < clippedTriangles.size(); b++) {
 					//TODO use the original triangle as an input, for interpolation
 					traverse(clippedTriangles.get(b), p.triangles2d.get(a), g2d, a, angle);
 				}
-			}
+			//}
 		}
 		
-		g2d.drawImage(finalImg, (int) p.startX, (int) p.startY, (int) p.innerWidth, (int) p.innerHeight, null);
 	}
 	
 	public void traverse(Double[][] clippedTriangle, Double[][] originalTriangle, Graphics2D g2d, int triangleNum, double angle) {	
+		int threadNumber = Integer.parseInt(Thread.currentThread().getName());
+		BufferedImage img = imageSections.get(threadNumber);
+		
 		Double[] temp;
 		
 		Integer[] p1 = new Integer[2];
@@ -380,13 +439,13 @@ public class Render extends JPanel implements Runnable{
 			
 			if(x1 > x2) {
 				for(int b = x1; b >= x2; b--) {
-					if(b >= 0 && b < finalImg.getWidth() && i >= 0 && i < finalImg.getHeight()) {
+					if(b >= corner1s.get(threadNumber)[0] && b < corner2s.get(threadNumber)[0] && i >= corner1s.get(threadNumber)[1] && i < corner2s.get(threadNumber)[1]) {
 						drawPoint(g2d, i, b, originalTriangle, triangleY, texCoords, angle);
 					}
 				}
 			} else if(x1 < x2) {
 				for(int b = x1; b <= x2; b++) {
-					if(b >= 0 && b < finalImg.getWidth() && i >= 0 && i < finalImg.getHeight()) {
+					if(b >= corner1s.get(threadNumber)[0] && b < corner2s.get(threadNumber)[0] && i >= corner1s.get(threadNumber)[1] && i < corner2s.get(threadNumber)[1]) {
 						drawPoint(g2d, i, b, originalTriangle, triangleY, texCoords, angle);
 					}
 				}
@@ -400,13 +459,13 @@ public class Render extends JPanel implements Runnable{
 			
 			if(x1 > x2) {
 				for(int b = x1; b >= x2; b--) {
-					if(b >= 0 && b < finalImg.getWidth() && i >= 0 && i < finalImg.getHeight()) {
+					if(b >= corner1s.get(threadNumber)[0] && b < corner2s.get(threadNumber)[0] && i >= corner1s.get(threadNumber)[1] && i < corner2s.get(threadNumber)[1]) {
 						drawPoint(g2d, i, b, originalTriangle, triangleY, texCoords, angle);
 					}
 				}
 			} else if(x1 < x2) {
 				for(int b = x1; b <= x2; b++) {
-					if(b >= 0 && b < finalImg.getWidth() && i >= 0 && i < finalImg.getHeight()) {
+					if(b >= corner1s.get(threadNumber)[0] && b < corner2s.get(threadNumber)[0] && i >= corner1s.get(threadNumber)[1] && i < corner2s.get(threadNumber)[1]) {
 						drawPoint(g2d, i, b, originalTriangle, triangleY, texCoords, angle);
 					}
 				}
@@ -420,8 +479,13 @@ public class Render extends JPanel implements Runnable{
 		Color color;
 		Double[] point;
 		Double[] uv;
-		double y;
 		point = new Double[] {(double) b, (double) i};
+		
+		int threadNumber = Integer.parseInt(Thread.currentThread().getName());
+		BufferedImage img = imageSections.get(threadNumber);
+		//For specific threads, not the absolute x (adjusted for corner1)
+		int x;
+		int y;
 	
 		
 		uv = interpolateCoords(triangleY, p.calculateBaryCoords(originalTriangle, point), texCoords, b, i);
@@ -449,7 +513,16 @@ public class Render extends JPanel implements Runnable{
 		color = new Color((int) (color.getRed() * lightLevel), (int) (color.getGreen() * lightLevel), (int) (color.getBlue() * lightLevel), 254);
 		g2d.setColor(color);
 		
-		finalImg.setRGB(b, i, color.getRGB());
+		x = (int) (b - corner1s.get(threadNumber)[0]);
+		y = (int) (i - corner1s.get(threadNumber)[1]);
+		
+		if(x >= img.getWidth()) {
+			x = img.getWidth() - 1;
+		}
+		if(y >= img.getHeight()) {
+			y = img.getHeight() - 1;
+		}
+		img.setRGB(x, y, color.getRGB());
 				
 	}
 	
@@ -526,7 +599,7 @@ public class Render extends JPanel implements Runnable{
 				baryCoords[1] / triangleY[1] + 
 				baryCoords[2] / triangleY[2]);
 		
-		Double bufferedY = zBuffer[b][i];
+		Double bufferedY  = zBuffer[b][i];
 		
 		if(bufferedY != null) {
 			if(bufferedY < y) {
@@ -535,7 +608,7 @@ public class Render extends JPanel implements Runnable{
 		}
 		
 		zBuffer[b][i] = y;
-				
+		
 		//y * (( t1.x / v1.y ) * b1 + ( t2.x / v2.y ) * b2 + ( t3.x / v3.y ) * b3)
 		u = y * ((texCoords[0][0] * baryCoords[0] / triangleY[0]) + 
 				(texCoords[1][0] * baryCoords[1] / triangleY[1]) + 
@@ -552,6 +625,7 @@ public class Render extends JPanel implements Runnable{
 				texCoords[1][1] * baryCoords[1]  + 
 				texCoords[2][1] * baryCoords[2]);
 		*/
+		
 		uv = new Double[] {u, v};
 		return uv;
 	}
